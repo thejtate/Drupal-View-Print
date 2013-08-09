3b447629a7ea58312938916dc9a4cd22838a6535
Josh Tate <thejtate@gmail.com>
2013-08-09 16:35:45 -0500

    Update 1.1

--- view-print.php
+++ view-print.php
@@ -1,6 +1,11 @@
 <?php
+  // More control to printing view
   $view = views_get_view('VIEWS_MACHINE_NAME');
   $view->set_display('DISPLAY_ID');
   print $view->get_title();
   print $view->preview('DISPLAY_ID');
+  
+  
+  // Easier method of printing view
+  print views_embed_view('VIEWS_MACHINE_NAME', 'DISPLAY_ID', $view_arg); 
 ?> 
